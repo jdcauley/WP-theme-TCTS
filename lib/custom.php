854456957fe2aaf18699915ec8f559ed539edd26
Jordan Cauley <jordan@cauley.co>
2014-03-13 15:47:15 -0400

    remove stupid shit

--- lib/custom.php
+++ lib/custom.php
@@ -3,129 +3,34 @@
  * Custom functions
  */
 
-if(function_exists("register_field_group"))
-{
-	register_field_group(array (
-		'id' => 'acf_surgery-types',
-		'title' => 'Surgery Types',
-		'fields' => array (
-			array (
-				'key' => 'field_5321f97413d9f',
-				'label' => 'Left Surgery Image',
-				'name' => 'left_surgery_image',
-				'type' => 'image',
-				'save_format' => 'url',
-				'preview_size' => 'full',
-				'library' => 'all',
+function custom_programs_posts() {
+  register_post_type( 'programs',
+		array(
+			'labels' => array(
+				'name' => __( 'Programs' ),
+				'singular_name' => __( 'programs' )
 			),
-			array (
-				'key' => 'field_5321fa5813da0',
-				'label' => 'Left Surgery Title',
-				'name' => 'left_surgery_title',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => 'Cardiac Surgery',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-			array (
-				'key' => 'field_5321face13da1',
-				'label' => 'Left Surgery Sub Title',
-				'name' => 'left_surgery_sub_title',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => 'Comprehensive Surgery...',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-			array (
-				'key' => 'field_5321fbb113da2',
-				'label' => 'Left Surgery Text',
-				'name' => 'left_surgery_text',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => 'Comprehensive surgical care is provided by...',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-			array (
-				'key' => 'field_5321fd5bca516',
-				'label' => 'Right Surgery Image',
-				'name' => 'right_surgery_image',
-				'type' => 'image',
-				'save_format' => 'url',
-				'preview_size' => 'full',
-				'library' => 'all',
-			),
-			array (
-				'key' => 'field_5321fd75ca517',
-				'label' => 'Right Surgery Title',
-				'name' => 'right_surgery_title',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => 'Cardiac Surgery',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-			array (
-				'key' => 'field_5321fd8dca518',
-				'label' => 'Right Surgery Sub Title',
-				'name' => 'right_surgery_sub_title',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => 'Comprehensive Surgery...',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-			array (
-				'key' => 'field_5321fda8ca519',
-				'label' => 'Right Surgery Text',
-				'name' => 'right_surgery_text',
-				'type' => 'text',
-				'default_value' => '',
-				'placeholder' => 'Comprehensive surgical care is provided by...',
-				'prepend' => '',
-				'append' => '',
-				'formatting' => 'html',
-				'maxlength' => '',
-			),
-		),
-		'location' => array (
-			array (
-				array (
-					'param' => 'page',
-					'operator' => '==',
-					'value' => '79',
-					'order_no' => 0,
-					'group_no' => 0,
-				),
-			),
-			array (
-				array (
-					'param' => 'post_type',
-					'operator' => '==',
-					'value' => 'post',
-					'order_no' => 0,
-					'group_no' => 1,
-				),
-			),
-		),
-		'options' => array (
-			'position' => 'normal',
-			'layout' => 'no_box',
-			'hide_on_screen' => array (
-			),
-		),
-		'menu_order' => 0,
-	));
+		'public' => true,
+		'has_archive' => true,
+		'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
+		'taxonomies' => array('category'),
+		)
+	);
 }
+add_action( 'init', 'custom_programs_posts' );
+
+function custom_physicians_posts() {
+  register_post_type( 'physicians',
+    array(
+      'labels' => array(
+        'name' => __( 'Physicians' ),
+        'singular_name' => __( 'physicians' )
+      ),
+    'public' => true,
+    'has_archive' => true,
+    'supports' => array('title', 'editor', 'thumbnail', 'custom-fields'),
+    'taxonomies' => array('category'),
+    )
+  );
+}
+add_action( 'init', 'custom_physicians_posts' );
