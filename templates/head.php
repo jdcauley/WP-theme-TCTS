c57339d3a43d05c2a36f6f4518ed96fe11d9bbad
Jordan Cauley <jordan@cauley.co>
2014-03-28 16:37:20 -0400

    Attempting to support ie8

--- templates/head.php
+++ templates/head.php
@@ -6,6 +6,10 @@
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
+  <!--[if lt IE 9]>
+   <script src="<?php echo get_template_directory_uri(); ?>assets/js/ie/html5shiv.js" type="text/javascript"></script>
+   <script src="<?php echo get_template_directory_uri(); ?>assets/js/ie/respond.min.js" type="text/javascript"></script>
+  <![endif]-->
   <?php wp_head(); ?>
 
   <link rel="alternate" type="application/rss+xml" title="<?php echo get_bloginfo('name'); ?> Feed" href="<?php echo esc_url(get_feed_link()); ?>">
